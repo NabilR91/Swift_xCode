f31153732b247518f9a2bdda52dd543de515c752
nabilreimer <j.nabil.r@web.de>
2022-11-07 12:44:18 +0100

    Aufgabe 2 fertig

---
+++ LieblingsApp(TableView Aufgabe 2)/LieblingsApp(TableView Aufgabe 2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LieblingsApp(TableView Aufgabe 2)
+//
+//  Created by Nabil Reimer on 07.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
